6a56c514cd56eb2b1c61e5b68235356c0009c6c7
Benjamin Hudgens <benjamin@benjamindavid.com>
2015-06-30 23:26:41 -0500

    Add prefix

--- logs.js
+++ logs.js
@@ -36,30 +36,35 @@ var log = {
     if (!this.enable.debug || !this.enable.logging) {
       return;
     }
+    arguments.unshift('[Debug]');
     console.log.apply(console, this.sanitizeArgs(arguments, colors.debug));
   },
   error: function() {
     if (!this.enable.error || !this.enable.logging) {
       return;
     }
+    arguments.unshift('[Error]');
     console.error.apply("Error:", this.sanitizeArgs(arguments, colors.error));
   },
   warn: function() {
     if (!this.enable.warn || !this.enable.logging) {
       return;
     }
+    arguments.unshift('[Warn] ');
     console.log.apply(console, this.sanitizeArgs(arguments, colors.warn));
   },
   input: function() {
     if (!this.enable.input || !this.enable.logging) {
       return;
     }
+    arguments.unshift('[Input]');
     console.log.apply(console, this.sanitizeArgs(arguments, colors.input));
   },
   info: function() {
     if (!this.enable.info || !this.enable.logging) {
       return;
     }
+    arguments.unshift('[Info] ');
     console.log.apply(console, this.sanitizeArgs(arguments, colors.info));
   }
 };
