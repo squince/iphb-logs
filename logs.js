76d697c85182fecbd9583c2533414eb48511dbd2
Benjamin Hudgens <benjamin@benjamindavid.com>
2015-06-30 23:33:40 -0500

    Spacing

--- logs.js
+++ logs.js
@@ -37,31 +37,31 @@ var log = {
     if (!this.enable.debug || !this.enable.logging) {
       return;
     }
-    console.log.apply(console, this.sanitizeArgs(arguments, colors.debug, '[Debug] '));
+    console.log.apply(console, this.sanitizeArgs(arguments, colors.debug, '[Debug]'));
   },
   error: function() {
     if (!this.enable.error || !this.enable.logging) {
       return;
     }
-    console.error.apply(console, this.sanitizeArgs(arguments, colors.error, '[Error] '));
+    console.error.apply(console, this.sanitizeArgs(arguments, colors.error, '[Error]'));
   },
   warn: function() {
     if (!this.enable.warn || !this.enable.logging) {
       return;
     }
-    console.log.apply(console, this.sanitizeArgs(arguments, colors.warn, '[Warn]  '));
+    console.log.apply(console, this.sanitizeArgs(arguments, colors.warn, '[Warn] '));
   },
   input: function() {
     if (!this.enable.input || !this.enable.logging) {
       return;
     }
-    console.log.apply(console, this.sanitizeArgs(arguments, colors.input, '[Input] '));
+    console.log.apply(console, this.sanitizeArgs(arguments, colors.input, '[Input]'));
   },
   info: function() {
     if (!this.enable.info || !this.enable.logging) {
       return;
     }
-    console.log.apply(console, this.sanitizeArgs(arguments, colors.info, '[Info]  '));
+    console.log.apply(console, this.sanitizeArgs(arguments, colors.info, '[Info] '));
   }
 };
 
