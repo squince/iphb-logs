585554e41814e9955a486bad3bae7ba0176a042b
Benjamin Hudgens <benjamin@benjamindavid.com>
2015-06-30 23:17:10 -0500

    Logic Error

--- logs.js
+++ logs.js
@@ -33,31 +33,31 @@ var log = {
     return args;
   },
   debug: function() {
-    if (!this.enable.debug && this.enable.logging) {
+    if (!this.enable.debug || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.debug));
   },
   error: function() {
-    if (!this.enable.error && this.enable.logging) {
+    if (!this.enable.error || !this.enable.logging) {
       return;
     }
     console.error.apply("Error:", this.sanitizeArgs(arguments, colors.error));
   },
   warn: function() {
-    if (!this.enable.warn && this.enable.logging) {
+    if (!this.enable.warn || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.warn));
   },
   input: function() {
-    if (!this.enable.input && this.enable.logging) {
+    if (!this.enable.input || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.input));
   },
   info: function() {
-    if (!this.enable.info && this.enable.logging) {
+    if (!this.enable.info || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.info));
