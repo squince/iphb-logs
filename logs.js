90a41456a9acd2ea016059c31aa3d17158d4f88e
Benjamin Hudgens <benjamin@benjamindavid.com>
2017-02-28 16:54:00 -0600

    ES6

--- logs.js
+++ logs.js
@@ -1,49 +1,45 @@
+"use strict";
 /**
  * Simple API for logging that gives a central place to control logging behavior
  */
 
 // Yoink in console colors (we use colors/safe so no overrides)
-var colors = require('colors/safe');
+import colors from 'colors/safe';
 
-// Set the colors for various log levels
-colors.setTheme({
-  input: 'grey',
-  verbose: 'cyan',
-  prompt: 'grey',
-  info: 'green',
-  data: 'blue',
-  help: 'cyan',
-  warn: 'yellow',
-  notice: 'yellow',
-  debug: 'white',
-  success: 'green',
-  fail: 'red',
-  error: 'red'
-});
+class Log {
 
-/**
- * Main export object
- * @type {Object}
- */
-var log = {
+  constructor() {
+    // Set the colors for various log levels
+    colors.setTheme({
+      input: 'grey',
+      verbose: 'cyan',
+      prompt: 'grey',
+      info: 'green',
+      data: 'blue',
+      help: 'cyan',
+      warn: 'yellow',
+      notice: 'yellow',
+      debug: 'white',
+      success: 'green',
+      fail: 'red',
+      error: 'red'
+    });
 
-  /**
-   * Settings
-   * @type {Object}
-   */
-  enable: {
-    logging: true,
-    debug: false,
-    verbose: false,
-    warn: true,
-    notice: true,
-    tests: false,
-    input: true,
-    info: true,
-    help: true,
-    data: true,
-    error: true
-  },
+    Object.assign(this.enable, {
+      logging: true,
+      debug: false,
+      verbose: false,
+      warn: true,
+      notice: true,
+      tests: false,
+      input: true,
+      info: true,
+      help: true,
+      data: true,
+      error: true
+    });
+  }
+
   /**
    * @function sanitizeArgs
    * @description Used for general formatting and color application of the log
@@ -54,7 +50,7 @@ var log = {
    * @param  {string} label             The label to prefix to this log output
    * @return {array}                    Cleaned up log output
    */
-  sanitizeArgs: function(originalArguments, color, label) {
+  sanitizeArgs(originalArguments, color, label) {
     var args = Array.prototype.slice.call(originalArguments);
     args.unshift(label);
     var c = args.length;
@@ -65,118 +61,117 @@ var log = {
       args[c] = color(args[c]);
     }
     return args;
-  },
+  }
   /**
    * @function debug
    * @description Debug Log Output
    */
-  debug: function() {
+  debug() {
     if (!this.enable.debug || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.debug, '[Debug]  '));
-  },
+  }
   /**
    * @function error
    * @description Error Log Output
    */
-  error: function() {
+  error() {
     if (!this.enable.error || !this.enable.logging) {
       return;
     }
     console.error.apply(console, this.sanitizeArgs(arguments, colors.error, '[Error]  '));
-  },
+  }
   /**
    * @function warn
    * @description Warn Log Output
    */
-  help: function() {
+  help() {
     if (!this.enable.help || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.help, '[Help]   '));
-  },
+  }
   /**
    * @function warn
    * @description Warn Log Output
    */
-  warn: function() {
+  warn() {
     if (!this.enable.warn || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.warn, '[Warn]   '));
-  },
+  }
   /**
    * @function notice
    * @description Notice Log Output
    */
-  notice: function() {
+  notice() {
     if (!this.enable.warn || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.warn, '[Notice] '));
-  },
+  }
   /**
    * @function input
    * @description Input Log Output
    */
-  input: function() {
+  input() {
     if (!this.enable.input || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.input, '[Input]  '));
-  },
+  }
   /**
    * @function verbose
    * @description Verbose Log Output
    */
-  verbose: function() {
+  verbose() {
     if (!this.enable.verbose || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.verbose, '[Verbose]'));
-  },
+  }
   /**
    * @function info
    * @description Info Log Output
    */
-  info: function() {
+  info() {
     if (!this.enable.info || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.info, '[Info]   '));
-  },
+  }
   /**
    * @function info
    * @description Info Log Output
    */
-  success: function() {
+  success() {
     if (!this.enable.tests) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.success, '[Success]'));
-  },
+  }
   /**
    * @function info
    * @description Info Log Output
    */
-  fail: function() {
+  fail() {
     if (!this.enable.tests) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.fail, '[Fail]   '));
-  },
+  }
   /**
    * @function data
    * @description Data Log Output
    */
-  data: function() {
+  data() {
     if (!this.enable.data || !this.enable.logging) {
       return;
     }
     console.log.apply(console, this.sanitizeArgs(arguments, colors.data, '[Data]   '));
   }
-};
+}
 
-// Expose the log functions
-module.exports = log;
+export default new Log();
